a6ddb1658a7bea8bc8dbb35abebb00742cbcdce6
knivlina <linwid3@gmail.com>
2022-05-12 16:49:44 +0200

    Setting up java environment, W.I.P.

---
+++ algoritmika_java/src/phraseStructure/PrepPhrase.java
@@ -0,0 +1,5 @@
+package phraseStructure;
+
+public class PrepPhrase {
+
+}
---
+++ algoritmika_java/src/phraseStructure/VerbPhrase.java
@@ -0,0 +1,10 @@
+package phraseStructure;
+
+public class VerbPhrase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
