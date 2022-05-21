389bc1262de4b31fcfb10355c9bdc00b9464d593
linwid3 <linwid3@gmail.com>
2022-05-21 20:55:41 +0200

    Removed unused libs

--- java/workspace/algoritmika/src/main/java/algoritmika/Main.java
+++ java/workspace/algoritmika/src/main/java/algoritmika/Main.java
@@ -1,8 +1,9 @@
 package algoritmika;
 
 public class Main {
+
 	public static void main(String[] args) {
-		System.out.println("Algoritmika is initialized");
+		System.out.println("Hello World");
 	}
 
 }
